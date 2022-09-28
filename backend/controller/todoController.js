cd6ee3511921e7a9d6126e245256c298ae8302e9
Shiv Kashyap <ssgkashyap@outlook.com>
2022-09-28 17:42:05 +0100

    Todo controller added and routes cleaned up

--- backend/controller/todoController.js
+++ backend/controller/todoController.js
@@ -1,14 +1,24 @@
-const getTodo = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+const getTodo = asyncHandler(async (req, res) => {
   res.send({ message: "Get Todos" });
-};
-const setTodo = (req, res) => {
-  res.send({ message: "Set Todos" });
-};
-const updateTodo = (req, res) => {
+});
+
+const setTodo = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add text");
+  }
+
+  res.send({ message: "Set todo" });
+});
+
+const updateTodo = asyncHandler(async (req, res) => {
   res.send({ message: `Update Todo ${req.params.id}` });
-};
-const deleteTodo = (req, res) => {
+});
+
+const deleteTodo = asyncHandler(async (req, res) => {
   res.send({ message: `Delete Todo ${req.params.id}` });
-};
+});
 
 module.exports = { getTodo, setTodo, updateTodo, deleteTodo };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 3000;
+
 const app = express();
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/todo", require("./routes/routes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
